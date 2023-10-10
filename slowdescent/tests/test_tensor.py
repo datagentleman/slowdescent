afcdd0be59be0da5d4872d6de8a66ed11701658e
damianu <damian@datagentleman.io>
2023-10-10 20:53:20 +0200

    extract tensor creation in tests

--- slowdescent/tests/test_tensor.py
+++ slowdescent/tests/test_tensor.py
@@ -3,25 +3,23 @@ import numpy as np
 
 from ..tensor import Tensor
 
+def create_tensors():
+    dim_2x2 = [[1, 1], [1, -1]]
+
+    sd1 = Tensor(dim_2x2)
+    sd2 = Tensor(dim_2x2)
+    
+    t1 = torch.tensor(dim_2x2)
+    t2 = torch.tensor(dim_2x2)
+    
+    return sd1, sd2, t1, t2    
+
+
 def test_tensor_add():
-    dim_2x2 = [[1, 1], [1, -1]]
-
-    sd1 = Tensor(dim_2x2)
-    sd2 = Tensor(dim_2x2)
-    
-    t1 = torch.tensor(dim_2x2)
-    t2 = torch.tensor(dim_2x2)
-    
+    sd1, sd2, t1, t2 = create_tensors()
     np.testing.assert_array_equal(sd1+sd2, t1+t2)
-    
-    
-def test_tensor_sub():
-    dim_2x2 = [[1, 1], [1, -1]]
+        
 
-    sd1 = Tensor(dim_2x2)
-    sd2 = Tensor(dim_2x2)
-    
-    t1 = torch.tensor(dim_2x2)
-    t2 = torch.tensor(dim_2x2)
-    
+def test_tensor_sub():
+    sd1, sd2, t1, t2 = create_tensors()      
     np.testing.assert_array_equal(sd1-sd2, t1-t2)
