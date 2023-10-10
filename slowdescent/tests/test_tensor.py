561186e71360581ba95cd202b90a48e32fdab199
damianu <damian@datagentleman.io>
2023-10-10 22:52:02 +0200

    add helper method for creating nd tensors

--- slowdescent/tests/test_tensor.py
+++ slowdescent/tests/test_tensor.py
@@ -1,39 +1,43 @@
 import torch
 import numpy as np
+import operator
+import functools
 
 from ..tensor import Tensor
 
-def create_tensors():
-    dim_2x2 = [[1, 1], [1, -1]]
 
-    sd1 = Tensor(dim_2x2)
-    sd2 = Tensor(dim_2x2)
+def create_tensors(*dims):
+    size = functools.reduce(operator.mul, dims)
+    data = np.arange(1, size+1).reshape(dims)
+     
+    sd1 = Tensor(data)
+    sd2 = Tensor(data)
     
-    t1 = torch.tensor(dim_2x2)
-    t2 = torch.tensor(dim_2x2)
+    t1 = torch.tensor(data)
+    t2 = torch.tensor(data)
     
     return sd1, sd2, t1, t2
 
 
 def test_tensor_add():
-    sd1, sd2, t1, t2 = create_tensors()
+    sd1, sd2, t1, t2 = create_tensors(2, 2)
     np.testing.assert_array_equal(sd1+sd2, t1+t2)
     np.testing.assert_array_equal(sd1+2.0, t1+2.0)
 
 
 def test_tensor_sub():
-    sd1, sd2, t1, t2 = create_tensors()      
+    sd1, sd2, t1, t2 = create_tensors(2, 2)      
     np.testing.assert_array_equal(sd1-sd2, t1-t2)
     np.testing.assert_array_equal(sd1-2, t1-2)
 
 
 def test_tensor_mul():
-    sd1, sd2, t1, t2 = create_tensors()
+    sd1, sd2, t1, t2 = create_tensors(2, 2)
     np.testing.assert_array_equal(sd1*sd2, t1*t2)
     np.testing.assert_array_equal(sd1*2, t1*2)
 
 
 def test_tensor_div():
-    sd1, sd2, t1, t2 = create_tensors()      
+    sd1, sd2, t1, t2 = create_tensors(2, 2)      
     np.testing.assert_array_equal(sd1/sd2, t1/t2)
     np.testing.assert_array_equal(sd1/2, t1/2)
