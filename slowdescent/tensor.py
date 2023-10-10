d761f72d05c3a57686db4d074944ab0fb17e0b60
damianu <damian@datagentleman.io>
2023-10-10 22:10:29 +0200

    tensor operations are working with numbers :tada:

--- slowdescent/tensor.py
+++ slowdescent/tensor.py
@@ -1,24 +1,36 @@
 from __future__ import annotations
 
 import numpy as np
-from typing import List
+from typing import List, Union
+from numbers import Number
 
+ND_DATA = Union['Tensor', Number, np.ndarray]
 
 class Tensor:
     def __init__(self, data: List|np.ndarray):
         self.data = np.array(data)
 
+
     def __array__(self) -> np.ndarray:
         return self.data
 
-    def __add__(self, t2:Tensor) -> Tensor:
-        return Tensor(self.data + t2.data)
-    
-    def __sub__(self, t2:Tensor) -> Tensor:
-        return Tensor(self.data - t2.data)
 
-    def __mul__(self, t2:Tensor) -> Tensor:
-        return Tensor(self.data * t2.data)
+    def __add__(self, t2: ND_DATA) -> Tensor:
+        t2 = t2.data if isinstance(t2, Tensor) else t2 
+        return Tensor(self.data + t2)
+
     
-    def __truediv__(self, t2:Tensor) -> Tensor:
-        return Tensor(self.data / t2.data)
+    def __sub__(self, t2: ND_DATA) -> Tensor:
+        t2 = t2.data if isinstance(t2, Tensor) else t2 
+        return Tensor(self.data - t2)
+
+
+    def __mul__(self, t2: ND_DATA) -> Tensor:
+        t2 = t2.data if isinstance(t2, Tensor) else t2 
+        return Tensor(self.data * t2)
+    
+
+    def __truediv__(self, t2: ND_DATA) -> Tensor:
+        t2 = t2.data if isinstance(t2, Tensor) else t2 
+        return Tensor(self.data / t2)
+    
--- slowdescent/tests/test_tensor.py
+++ slowdescent/tests/test_tensor.py
@@ -12,24 +12,28 @@ def create_tensors():
     t1 = torch.tensor(dim_2x2)
     t2 = torch.tensor(dim_2x2)
     
-    return sd1, sd2, t1, t2    
+    return sd1, sd2, t1, t2
 
 
 def test_tensor_add():
     sd1, sd2, t1, t2 = create_tensors()
     np.testing.assert_array_equal(sd1+sd2, t1+t2)
-        
+    np.testing.assert_array_equal(sd1+2.0, t1+2.0)
+
 
 def test_tensor_sub():
     sd1, sd2, t1, t2 = create_tensors()      
     np.testing.assert_array_equal(sd1-sd2, t1-t2)
+    np.testing.assert_array_equal(sd1-2, t1-2)
 
 
 def test_tensor_mul():
-    sd1, sd2, t1, t2 = create_tensors()          
+    sd1, sd2, t1, t2 = create_tensors()
     np.testing.assert_array_equal(sd1*sd2, t1*t2)
+    np.testing.assert_array_equal(sd1*2, t1*2)
 
 
 def test_tensor_div():
     sd1, sd2, t1, t2 = create_tensors()      
     np.testing.assert_array_equal(sd1/sd2, t1/t2)
+    np.testing.assert_array_equal(sd1/2, t1/2)
