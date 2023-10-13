e9603a87a22f781365f961566cb325128e317ac5
damianu <damian@datagentleman.io>
2023-10-13 21:37:11 +0200

    model is running

--- slowdescent/model.py
+++ slowdescent/model.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
-from typing import List
+
+from typing  import List
+from .tensor import Tensor
 
 
 class Model:
     def __init__(self):
         self.layers: List = []
-                        
+        
+        
     def add(self, layer) -> Model:
         self.layers.append(layer)
         return self
+        
+
+    def run(self, batch: Tensor):
+        input: Tensor = batch
+        
+        for layer in self.layers:
+            input = layer.forward(input)
+            
--- slowdescent/tests/test_model.py
+++ slowdescent/tests/test_model.py
@@ -1,11 +1,25 @@
+import torch
 import numpy as np
 
 from ..layers.activations import RELU
 from ..layers.linear import Linear
+from ..tensor import Tensor
 from ..model import Model
+
 
 def test_model_add():
     m = Model()
     m.add(Linear()).add(RELU())
     
     assert(len(m.layers) == 2)
+
+
+def test_model_run():
+    m = Model()
+    data = np.ndarray((2,2))
+    
+    linear = Linear(2, 2)
+    input = Tensor(data.copy())
+    
+    m.add(linear).run(input)
+    assert(linear.z != None)
